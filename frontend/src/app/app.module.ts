967d2dcc336b4ac4ccc25333dfe0be3a70c356bb
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-10-08 14:53:42 -0300

    Atualizacao de produto - parte 1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -44,7 +45,8 @@ registerLocaleData(localePt);
     RedDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
