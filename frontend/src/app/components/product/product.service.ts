de091426b23fb548ec3a8027998ca9effddda5fb
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-10-08 10:07:31 -0300

    Obter produto

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -26,4 +26,8 @@ export class ProductService {
     return this.http.post<Product>(this.baseURL, product)
   }
 
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseURL)
+  }
+
 }
